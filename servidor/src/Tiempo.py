5baa65d4595309c2b6cbb884f165c3bbb9fe7553
kecorz <47731260+kecorz@users.noreply.github.com>
2019-04-10 21:18:07 -0500

    Create Tiempo.py

---
+++ servidor/src/Tiempo.py
@@ -0,0 +1,16 @@
+import time
+
+def tiempo_ejecucion(message):
+    def decorador(funcion):
+        def wrapped():
+
+            tiempo_inicio = time.time()
+
+            funcion(message[0],message[1],message[2]) #3 argumentos 
+
+            tiempo_final = time.time()
+
+            print("Tiempo ejecucion: ", tiempo_final-tiempo_inicio)
+
+        return wrapped
+    return decorador
